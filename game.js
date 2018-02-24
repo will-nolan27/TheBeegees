97ecf70ef30e1cae002d22757ce4384decc27ab8
Will <apricotsenator@gmail.com>
2018-02-24 09:56:38 -0500

    pretty-good-max-min-vals-off

--- game.js
+++ game.js
@@ -1,98 +1,93 @@
-$(function () {
-   
+$(document).ready(function () {
 
-$("#Start").on("click", function () {
-            var total = 0
-            $("#points").text(total);
-            var winsTotal = 1
+    var broPoints = [, , , ];
+    var total = 0;
+    var winsTotal = 0;
+    var lossesTotal = 0;
+    var beatMe = "";
+
+    function start() {
+        total = 0
+        $("#points").text(total);
+        winsTotal = 0
+        $("#wins").text(winsTotal);
+        lossesTotal = 0
+        $("#losses").text(lossesTotal);
+
+        randomNum = function getRandomIntInclusive(min, max) {
+            min = Math.ceil(19);
+            max = Math.floor(120);
+            return Math.floor(Math.random() * (max - min + 1)) + min; //The maximum is inclusive and the minimum is inclusive 
+          }
+        beatMe = randomNum();
+        $("#moves").text(beatMe);
+
+        for (var i = 0; i < 4; i++) {
+            var broValue = Math.floor(Math.random() * (12 -1)) + 1;
+            broPoints[i] = broValue;
+        };
+    };
+
+    function reset() {
+        total = 0;
+        beatMe= 0;
+        $("#points").text(total);
+        randomNum = function getRandomIntInclusive(min, max) {
+            min = Math.ceil(19);
+            max = Math.floor(120);
+            return Math.floor(Math.random() * (max - min + 1)) + min; //The maximum is inclusive and the minimum is inclusive 
+          }
+        beatMe = randomNum();
+        $("#moves").text(beatMe);
+
+        for (var i = 0; i < 4; i++) {
+            var broValue = Math.floor(Math.random() * 12) + 1;
+            broPoints[i] = broValue;
+        };
+
+    }
+
+   function remo () {
+        $('#toggle').toggleClass('hid')};
+
+    $("#Rules").on("click",remo);
+    $("#Start").on("click", start);
+
+    $("#barry").on("click", function () {
+        total = (total + broPoints[0]);
+        $("#points").text(total);
+        comp();
+    });
+
+    $("#robin").on("click", function () {
+        total = (total + broPoints[1]);
+        $("#points").text(total);
+        comp();
+    });
+
+    $("#maurice").on("click", function () {
+        total = (total + broPoints[2]);
+        $("#points").text(total);
+        comp();
+    });
+
+    $("#andy").on("click", function () {
+        total = (total + broPoints[3]);
+        $("#points").text(total);
+        comp();
+    });
+
+    function comp() {
+        if (total === beatMe) {
+            winsTotal++;
             $("#wins").text(winsTotal);
-            var lossesTotal = 2
+            reset();
+
+        } else if (total > beatMe) {
+            lossesTotal++;
             $("#losses").text(lossesTotal);
-            var beatMe = "";
-            var random = Math.floor(Math.random() * 120) + 19;
-            beatMe = random;
-            $("#moves").text(beatMe);
-            
+            reset();
+        };
+    };
 
-            var broPoints = [, , , ];
-            for (var i = 0; i < 4; i++) {
-                var broValue = Math.floor(Math.random() * 12) + 1;
-                broPoints[i] = broValue;
-
-            }
-
-                $("#barry").on("click", function () {
-                    total = (total + broPoints[0]);
-                    $("#points").text(total);
-                    if (total === beatMe) {
-                        winsTotal++;
-                        $("#wins").text(winsTotal);
-                        
-                    } else if (total > beatMe) {
-                        lossesTotal++;
-                        $("#losses").text(lossesTotal);
-                        
-                    } else {
-                        return;
-                        
-                    }
-                });
-
-                $("#robin").on("click", function () {
-                    total = (total + broPoints[1]);
-                    $("#points").text(total);
-                    if (total === beatMe) {
-                        winsTotal++;
-                        $("#wins").text(winsTotal);
-                        
-                    } else if (total > beatMe) {
-                        lossesTotal++;
-                        $("#losses").text(lossesTotal);
-                        
-                    } else {
-                        return;
-                        
-                    }
-                });
-
-                $("#maurice").on("click", function () {
-                    total = (total + broPoints[2]);
-                    $("#points").text(total);
-                    if (total === beatMe) {
-                        winsTotal++;
-                        $("#wins").text(winsTotal);
-                        
-                    } else if (total > beatMe) {
-                        lossesTotal++;
-                        $("#losses").text(lossesTotal);
-                        
-                    } else {
-                        return;
-                        
-                    }
-                });
-
-                $("#andy").on("click", function () {
-                    total = (total + broPoints[3]);
-                    $("#points").text(total);
-                    if (total === beatMe) {
-                        winsTotal++;
-                        $("#wins").text(winsTotal);
-                        
-                    } else if (total > beatMe) {
-                        lossesTotal++;
-                        $("#losses").text(lossesTotal);
-                        
-                    } else {
-                        return;
-                        
-                    }
-                });
-                
-
-
-
-            });
-            
-            
-        });
+});
